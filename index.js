61b20874590dccfe1030d2634ba43f8a7524e265
mykelgozie <mykelgozie@gmail.com>
2026-05-02 23:26:29 +0100

    update service

--- index.js
+++ index.js
@@ -52,6 +52,4 @@ app.get("/api/products", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Backend running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`🚀 Backend on http://localhost:${PORT}`));
